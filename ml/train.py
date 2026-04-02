f7aafa4221aa10af0637c4cf79d44c9ca80604cf
LollaYasasviAnirudh <anirudh.yasasvi@techoptima.ai>
2026-04-02 16:12:51 +0530

    Data cleaning and preprocessing completed

--- ml/train.py
+++ ml/train.py
@@ -1,4 +1,22 @@
 import pandas as pd
 
+# Load data
 df = pd.read_csv("data/dataset.csv")
+
+# Fix TotalCharges
+df['TotalCharges'] = pd.to_numeric(df['TotalCharges'], errors='coerce')
+
+# Drop missing
+df = df.dropna()
+
+# Drop ID
+df = df.drop('customerID', axis=1)
+
+# Convert target
+df['Churn'] = df['Churn'].map({'Yes': 1, 'No': 0})
+
+# Encode categorical
+df = pd.get_dummies(df, drop_first=True)
+
+print("Cleaned Data Shape:", df.shape)
 print(df.head())
